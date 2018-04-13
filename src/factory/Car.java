df965c1a808df29f033339cbb2a33f975aac806a
AdamZubrycki <zazubrycki@gmail.com>
2018-04-13 14:16:46 +0200

    Repleasing with constructor

--- src/factory/Car.java
+++ src/factory/Car.java
@@ -3,13 +3,23 @@ package factory;
 import java.util.Calendar;
 
 public class Car {
-    //instance variables
-    String make;
-    String model;
-    int year;
-    boolean isNew = true;
-    double miles;
-    String owner;
+    //instance variables - private after constructor use (released)
+    private String make;
+    private String model;
+    private int year;
+    private boolean isNew = true;
+    private double miles;
+    private String owner;
+
+    //constructor
+    public Car(String make, String model, int year, boolean isNew, double miles, String owner) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.isNew = isNew;
+        this.miles = miles;
+        this.owner = owner;
+    }
 
     public void sell(String NewOwner) {
         owner = NewOwner;
--- src/factory/Main.java
+++ src/factory/Main.java
@@ -8,14 +8,9 @@ package factory;
 public class Main {
 
     public static void main(String[] args) {
-        Car myCar = new Car();
+        //with constructor call
+        Car myCar = new Car("Toyota","Aygo",2011,true,40000,"Adam");
 
-        myCar.make = "Toyota";
-        myCar.model = "Aygo";
-        myCar.year = 2011;
-        myCar.isNew=true;
-        myCar.miles=40000;
-        myCar.owner="Adam";
         myCar.isOld();
         myCar.sell("Lukas");
     }
